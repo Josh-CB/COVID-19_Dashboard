5c1e25072db9e6af5f4ceb9dd4652225301e864e
hillsj4 <Lolabola1@>
2020-04-01 18:51:08 +0100

    Added data and python script to retrieve and extract data to repo

---
+++ data/getData.py
@@ -0,0 +1,78 @@
+'''This python script retrieves new data from the ECDC, saves it to a file
+   and then open the file, reads data and creates a new file of required data for the
+   web app. It's scuffed but works.
+'''
+import requests
+import json
+import datetime
+
+def getDateAWeekAgo(date):
+    '''
+    1) take date as string
+    2) convert date string to datetime object
+    3) find date 7 days ago
+    4) convert back to string format
+    '''
+    datetimeDate = datetime.datetime.strptime(date, '%d/%m/%Y')
+    datetimeSevenDaysAgo = datetimeDate - datetime.timedelta(days=7)
+    minimumDate = datetime.datetime(2019, 12, 31)
+    if datetimeSevenDaysAgo < minimumDate:
+        datetimeSevenDaysAgo = datetime.datetime(2019, 12, 31)
+    stringSevenDaysAgo = datetimeSevenDaysAgo.strftime("%d/%m/%Y")
+    return stringSevenDaysAgo
+
+try:
+    url = "https://opendata.ecdc.europa.eu/covid19/casedistribution/json/"
+    data = requests.get(url)
+    f = open("raw_data.json", "w")
+    f.write(data.text)
+    f.close()
+    print("retrieved and saved.")
+except Exception as e:
+    print("Exception: ", e)
+
+f = open("raw_data.json", "r")
+raw_data = f.read()
+data = json.loads(raw_data)["records"]
+f.close()
+toWrite_raw = {}
+
+countriesToInclude = ["GBR", "CHN", "ITA"]
+for record in data:
+    if record["countryterritoryCode"] in countriesToInclude:
+        country = record["countryterritoryCode"]
+        newDeaths = int(record["deaths"])
+        if record["countryterritoryCode"] in toWrite_raw.keys():
+            toWrite_raw[record["countryterritoryCode"]][record["dateRep"]] = {"newDeaths": newDeaths}
+        else:
+            toWrite_raw[record["countryterritoryCode"]] = {record["dateRep"]: {"newDeaths": newDeaths}}
+
+for country in countriesToInclude:
+    totalDeaths = 0
+    listDates = list(toWrite_raw[country].items())
+    listDates.sort(key=lambda x: datetime.datetime.strptime(x[0], '%d/%m/%Y'))
+    for pair in listDates:
+        date = pair[0]
+        dateDict = pair[1]
+        totalDeaths = totalDeaths + dateDict["newDeaths"]
+        toWrite_raw[country][date]["totalDeathsToDate"] = totalDeaths
+
+for country in toWrite_raw:
+    for date in toWrite_raw[country]:
+        deathsUpToToday = toWrite_raw[country][date]["totalDeathsToDate"]
+        dateWeekAgo = getDateAWeekAgo(date)
+        deathsAWeekAgo = toWrite_raw[country][dateWeekAgo]["totalDeathsToDate"]
+        weeklyDeaths = deathsUpToToday - deathsAWeekAgo
+        toWrite_raw[country][date]["totalDeathsInLastWeek"] = weeklyDeaths
+
+print(toWrite_raw)
+toWrite = json.dumps(toWrite_raw)
+
+f = open("data.json", "w")
+f.write(toWrite)
+f.close()
+
+f = open("lastUpdate.txt", "w")
+lastUpdated = datetime.datetime.now().strftime("%d/%m/%Y %H:%M") + " UTC+01:00"
+f.write(lastUpdated)
+f.close()
